21e54946418e04928326ca228e18890eb08cabdd
Riley Tudbury <tudbury@gmail.com>
2020-01-29 14:56:08 -0700

    Added win tracker and reset button

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let buttonGroupElem = document.getElementById("button-group")
 let resultFrameElem = document.getElementById("result-frame")
 let gameFrameElemLeft = document.getElementById("game-frame-left")
 let gameFrameElemRight = document.getElementById("game-frame-right")
+let playerWins = 0
+let computerWins = 0
 
 
 
@@ -13,6 +15,7 @@ function play(playerChoice) {
 
   computerRoll()
   drawGameFrame(playerChoice, computerChoice)
+  drawWins()
 
   console.log(`Computer has chosen ${computerChoice}`)
   console.log(`You have chosen ${playerChoice}`)
@@ -25,26 +28,51 @@ function play(playerChoice) {
       case "rock":
         if (computerChoice == "paper") {
           resultFrameElem.textContent = "Sorry, you lose."
+          computerWins++
           break;
         }
         resultFrameElem.textContent = "You won!"
+        playerWins++
         break;
       case "paper":
         if (computerChoice == "scissors") {
           resultFrameElem.textContent = "Sorry, you lose."
+          computerWins++
           break;
         }
         resultFrameElem.textContent = "You won!"
+        playerWins++
         break;
       case "scissors":
         if (computerChoice == "rock") {
           resultFrameElem.textContent = "Sorry, you lose."
+          computerWins++
           break;
         }
         resultFrameElem.textContent = "You won!"
+        playerWins++
         break;
     }
   }
+}
+
+// Tracks wins for player and computer
+
+
+function drawWins() {
+
+  document.getElementById("player-wins").textContent = `${playerWins}`
+  document.getElementById("computer-wins").textContent = `${computerWins}`
+
+}
+
+
+// Resets the wins counter
+function winsReset() {
+  playerWins = 0
+  computerWins = 0
+
+  drawWins()
 }
 
 
